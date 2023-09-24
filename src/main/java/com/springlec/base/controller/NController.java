758aa53dee4b992085cc62bd75bff797fd627eba
Oh-Kang94 <okh19941994@naver.com>
2023-09-24 21:43:16 +0900

    init commit

---
+++ src/main/java/com/springlec/base/controller/NController.java
@@ -0,0 +1,18 @@
+package com.springlec.base.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class NController {
+
+	@RequestMapping("/")
+	public String mainView() throws Exception {
+		return "redirect:loginCheck";
+	}
+
+	@RequestMapping("/index")
+	public String indexView() throws Exception {
+		return "index";
+	}
+}
